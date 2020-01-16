9b1f0ecbcee1cef419c39e96cfeef2f3cef364be
Gabriellvs60 <gabriellvs60@gmail.com>
2020-01-16 16:52:38 -0300

    Web quase finalizado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes.js');
-
+const cors = require('cors');
 const app = express();
 
 mongoose.connect('mongodb+srv://astolfo:1234@cluster0-i9cmz.mongodb.net/week10?retryWrites=true&w=majority',{
@@ -9,6 +9,7 @@ mongoose.connect('mongodb+srv://astolfo:1234@cluster0-i9cmz.mongodb.net/week10?r
     useUnifiedTopology: true,
 });
 
+app.use(cors({origin: 'http://localhost:3000'}))
 app.use(express.json());
 app.use(routes);
 
